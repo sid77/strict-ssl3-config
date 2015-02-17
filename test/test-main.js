05823f956f12f8436988e1f26d9a453cfd07ea95
Marco Bonetti <sid77@slackware.it>
2015-02-17 22:31:44 +0000

    Test whitelisted preferences are set true upon starting Firefox

--- test/test-main.js
+++ test/test-main.js
@@ -1,12 +1,23 @@
+var prefs = require("sdk/preferences/service");
 var main = require("./main");
 
+/**
+ * Test that whitelisted preferences are set true upon starting Firefox.
+ *
+ */
 exports["test main"] = function(assert) {
-  assert.pass("Unit test running!");
-};
-
-exports["test main async"] = function(assert, done) {
-  assert.pass("async Unit test running!");
-  done();
+  var whitelist = [
+    "security.ssl.require_safe_negotiation",
+    "security.ssl.treat_unsafe_negotiation_as_broken",
+    "security.ssl3.rsa_aes_256_sha",
+    "security.ssl3.ecdhe_rsa_aes_256_sha",
+    "security.ssl3.rsa_des_ede3_sha",
+    "security.ssl3.dhe_rsa_aes_256_sha",
+    "security.ssl3.ecdhe_rsa_aes_128_gcm_sha256",
+  ];
+  whitelist.forEach(function(preference) {
+    assert.equal(prefs.get(preference), true);
+  });
 };
 
 require("sdk/test").run(exports);
