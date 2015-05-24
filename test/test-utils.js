afa32f3805267dad70fbf9de73e5d7919c30a76c
Marco Bonetti <sid77@slackware.it>
2015-05-24 12:26:18 +0200

    Remove security.ssl3.dhe_rsa_aes_256_sha from tests whitelist as well

--- test/test-utils.js
+++ test/test-utils.js
@@ -15,7 +15,6 @@ exports["test utils"] = function(assert) {
     "security.ssl3.rsa_aes_256_sha",
     "security.ssl3.ecdhe_rsa_aes_256_sha",
     "security.ssl3.rsa_des_ede3_sha",
-    "security.ssl3.dhe_rsa_aes_256_sha",
     "security.ssl3.ecdhe_rsa_aes_128_gcm_sha256",
   ];
 
